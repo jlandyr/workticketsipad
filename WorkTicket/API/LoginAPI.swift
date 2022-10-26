caf65c5dbe0981706ce47ec85b84b60e90aa9658
Juan Landy <jlandyr@gmail.com>
2022-10-27 01:39:20 +0200

    validateUser always returns a valid login

--- WorkTicket/API/LoginAPI.swift
+++ WorkTicket/API/LoginAPI.swift
@@ -10,12 +10,7 @@ import Foundation
 final class LoginAPI : LoginManager {
     func validateUser(withUserName userName: String, password: String, onComplete: @escaping (Result<Void, Error>) -> Void) {
         DispatchQueue.main.async {
-            if userName == "isucorp", password == "isucorp" {
-                onComplete(.success(Void()))
-            } else {
-                onComplete(.failure(NSError(domain: "username or password incorrects", code: 404)))
-            }
-            
+            onComplete(.success(Void()))
         }
     }
 }
