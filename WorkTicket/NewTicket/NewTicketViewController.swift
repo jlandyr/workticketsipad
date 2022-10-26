9d98d86145e2d9e310b36167ce0d4db9f672da19
Juan Landy <jlandyr@gmail.com>
2022-10-27 01:37:15 +0200

    New ticket creation with userName or optional Ticket?

--- WorkTicket/NewTicket/NewTicketViewController.swift
+++ WorkTicket/NewTicket/NewTicketViewController.swift
@@ -9,17 +9,28 @@ import UIKit
 
 class NewTicketViewController: UIViewController {
 
+    @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var textName: UITextField!
     @IBOutlet weak var textAddress: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    //
     var callback : ((Ticket) -> Void)?
-    var identifier : String?
-    
-    private var selectedDate : Date?
+    private var selectedDate = Date()
+    var object : (identifier: String, ticket:Ticket?)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTicket()
+    }
+    
+    private func setTicket(){
+        guard let ticket = object?.ticket else {return}
+        labelTitle.text = "Update ticket"
+        textName.text = ticket.name
+        textAddress.text = ticket.address
+        datePicker.date = ticket.date
+        selectedDate = ticket.date
     }
     
     @IBAction func dateDidChange(_ sender: Any) {
@@ -27,8 +38,8 @@ class NewTicketViewController: UIViewController {
     }
     
     @IBAction func didSelectSave(_ sender: Any) {
-        if let name = textName.text, !name.isEmpty, let address = textAddress.text, !address.isEmpty, let date = selectedDate, let id = identifier {
-            callback?(Ticket(name: name, address: address, date: date, identifier: id))
+        if let name = textName.text, !name.isEmpty, let address = textAddress.text, !address.isEmpty, let id = object?.identifier {
+            callback?(Ticket(name: name, address: address, date: selectedDate, identifier: id, key: object?.ticket?.key ?? 0))
             dismiss(animated: true)
         }
     }
